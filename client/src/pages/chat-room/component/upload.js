6fdd61b020b8c04b65804a5915b882501a11e4a9
陈桥格 <qiaoge.chen@ikang.com>
2020-03-23 13:49:05 +0800

    进度条，上传文件方法优化，进度条待优化

--- client/src/pages/chat-room/component/upload.js
+++ client/src/pages/chat-room/component/upload.js
@@ -9,12 +9,12 @@ import ClipboardJS from 'clipboard'
 // import worker_script from '/hash.js';
 var myWorker = new Worker('/hash.js')
 
-let a = 0
 function UploadFile (props) {
   const { sendMes, changeProcess } = props
   const [fileDatas, setFileDatas] = useState()
   const [chunkFileMes, setChunkFileMes] = useState({})
   const [uploading, setUploading] = useState(false)
+  hackChangeProcess = changeProcess
   /**
    *  选择文件变化，更新数据
    *  */
@@ -40,7 +40,7 @@ function UploadFile (props) {
         Toast.info('请先选择文件')
         return
       }
-      // receivedNum = 0
+      changeProcess(0)
       if (fileDatas && fileDatas.type.includes('image')) {
         sendMes('img')
       } else {
@@ -87,13 +87,20 @@ function UploadFile (props) {
           Toast.info('文件秒传成功，文件已存在了！')
           return
         }
+        // hack for 取值，因为setState异步，为了取到最新state
+        let latestChunkFileMes = {
+          splitFileList: chunkLists,
+          chunkNumber,
+          chunkSize,
+          fileHash
+        }
         // ??待优化,取值有问题？？
-        console.log('chunkFileMes:',chunkFileMes)
+        console.log('chunkFileMes:', chunkFileMes, latestChunkFileMes)
+        changeProcess(0) // 进度重置
         // 切片上传功能函数
         uploadFunc(
-          uploadedList, chunkLists, chunkNumber, fileHash, chunkSize,
-          fileName
-        )
+          uploadedList, latestChunkFileMes,
+          fileName)
       }
     },
     [sendMes, fileDatas, chunkFileMes, changeProcess]
@@ -114,10 +121,10 @@ function UploadFile (props) {
     })
   }
   //
-  const testPercent = useCallback(() => {
-    console.log(a)
-    changeProcess(a++)
-  }, [changeProcess])
+  // const testPercent = useCallback(() => {
+  //   console.log(a)
+  //   changeProcess(a++)
+  // }, [changeProcess])
   useEffect(() => {
     console.log('didMount?')
     copyFunc('.copyText')
@@ -148,13 +155,13 @@ function UploadFile (props) {
           <Button className='goOn' inline onClick={() => sendFiles('continue')}>
             恢复
           </Button>
+          <Button inline
+            className='copyText'
+            data-clipboard-text="Just because you can doesn't mean you should — clipboard.js">
+            copy
+          </Button>
         </div>
       )}
-      <span
-        className='copyText'
-        data-clipboard-text="Just because you can doesn't mean you should — clipboard.js">
-        copy
-      </span>
       {/* <button class="btn" data-clipboard-action="cut" data-clipboard-target="#bar">
         Cut to clipboard
       </button> */}
@@ -167,7 +174,7 @@ export default UploadFile
  *文件切片 ,默认100Kb
  * @params files--目标文件，chunkSize--切片大小
  *  */
-function createFileChunk (files, chunkSize = 10000 * 1024) {
+function createFileChunk (files, chunkSize = 1000 * 1024) {
   const fileChunkList = []
   // 向上取整
   const length = Math.ceil(files.size / chunkSize)
@@ -219,13 +226,13 @@ function calculateHash (fileChunkList, changeProcess, setUploading) {
  * @param {*} fileHash 文件hash（spark-md5）
  * @param {*} chunkSize 切片大小
  */
-async function uploadFunc (uploadedList = [], splitFileList, chunkNumber, fileHash, chunkSize, fileName) {
-  // const { splitFileList, chunkNumber, fileHash, chunkSize } = chunkFileMes
+async function uploadFunc (uploadedList = [], chunkFileMes, fileName) {
+  const { splitFileList, chunkNumber, fileHash, chunkSize } = chunkFileMes
   // 遍历切片集合，
-  const Lists = splitFileList.filter(({ hash }) => !uploadedList.includes(hash))
+  const Lists = splitFileList && splitFileList.filter(({ hash }) => !uploadedList.includes(hash))
   console.log(splitFileList, uploadedList, Lists)
   // 注意此处的取值，确保了顺序
-  const fileChunkList = Lists.map(({ chunk, hash, index }) => {
+  const fileChunkList = Lists.length > 0 && Lists.map(({ chunk, hash, index }) => {
     const obj = new FormData()
     obj.append('chunk', chunk) // 片文件
     // hash码，标识文件片？？？
@@ -238,9 +245,10 @@ async function uploadFunc (uploadedList = [], splitFileList, chunkNumber, fileHa
     // 生成文件hash
     return { obj }
   })
+  hackCurChunkList = splitFileList
   // 切片文件集合挨个发送给后端
   const fetchList = fileChunkList.map(async ({ obj }, index) => {
-    await uploadFile(obj, processCalc,splitFileList[index])
+    await uploadFile(obj, processCalc, splitFileList[index])
     // 上传进度
   })
   await Promise.all(fetchList)
@@ -249,11 +257,20 @@ async function uploadFunc (uploadedList = [], splitFileList, chunkNumber, fileHa
   const fileType = fileName.slice(fileName.lastIndexOf('.'), fileName.length)
   await mergeFileFunc(fileHash + fileType, chunkSize, fileName)
 }
+var hackCurChunkList = []
+var hackChangeProcess
 // 计算文件上传进度
 function processCalc (val) {
   // 当前片的进度
-  console.log(val)
-  // return processCalc
+  // console.log(val,hackCurChunkList)
+  let process = 0
+  const fileLength = hackCurChunkList.length
+  hackCurChunkList.forEach(item => {
+    process += item.percentage
+  });
+  let proVal = Number(process / fileLength)
+  console.log(proVal)
+  hackChangeProcess(proVal)
 }
 /**
  * 文件秒传：向服务端验证文件是否上传
@@ -276,14 +293,15 @@ function pauseUpload (params) {
 }
 // 恢复上传??????合并到发送按钮上，否则是拆分上传方法
 async function continueUpload (chunkFileMes, fileName) {
-  const { splitFileList, chunkNumber, fileHash, chunkSize } = chunkFileMes
+  // const { splitFileList, chunkNumber, fileHash, chunkSize } = chunkFileMes
+  const { fileHash } = chunkFileMes
   const { uploadedList, shouldUpload } = await verifyUpload(fileName, fileHash)
   if (!shouldUpload) {
     Toast.info('文件秒传成功，文件已存在了！')
     return
   }
   await uploadFunc(
-    uploadedList, splitFileList, chunkNumber, fileHash, chunkSize,
+    uploadedList, chunkFileMes,
     fileName,
   )
   return uploadedList
